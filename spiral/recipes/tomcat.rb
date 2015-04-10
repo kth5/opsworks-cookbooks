65a324fc0a606e49d9144833b717a2117352afb1
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-10 15:16:30 +0800

    extract tomcat to /opt

--- spiral/recipes/tomcat.rb
+++ spiral/recipes/tomcat.rb
@@ -2,5 +2,9 @@ include_recipe 'apt'
 include_recipe 'spiral::default'
 
 execute 'tomcat_download' do
-  command "/usr/bin/wget -O /tmp/tomcat.tar.gz -q #{node['spiral']['tomcat']['url']}"
+  command "wget -O /tmp/tomcat.tar.gz -q #{node['spiral']['tomcat']['url']}"
+end
+
+execute 'tomcat_extract' do
+  command "tar xzf /tmp/tomcat.tar.gz -C /opt/"
 end
