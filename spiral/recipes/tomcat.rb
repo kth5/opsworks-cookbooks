508475b842dbf7e83d5da1a32dae7d98e1c20e5d
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-10 16:14:17 +0800

    misc modifications

--- spiral/recipes/tomcat.rb
+++ spiral/recipes/tomcat.rb
@@ -1,25 +1,28 @@
 include_recipe 'apt'
 include_recipe 'spiral::default'
 
-dl_lcoation = "/opt/tomcat.tar.gz"
+dl_location = "/opt/tomcat-#{node['spiral']['tomcat']['version']}.tar.gz"
+tomcat_path = "/opt/tomcat"
 
 remote_file dl_location do
   source node['spiral']['tomcat']['url']
+  not_if { ::File.exists?(dl_location) }
 end
 
 execute 'tomcat_extract' do
   command 'tar xzf #{dl_location} -C /opt'
+  not_if { ::File.exists?("/opt/apache-tomcat-#{node['spiral']['tomcat']['version']}") }
 end
 
-link '/opt/tomcat' do
+link tomcat_path do
   to "/opt/apache-tomcat-#{node['spiral']['tomcat']['version']}"
+  not_if { ::File.exists?(tomcat_path) }
 end
 
 directory "/opt/tomcat" do
   owner 'tomcat'
   group node['spiral']['users']['group']
   mode '0755'
-  action :create
 end
 
 #supervisor_service 'tomcat' do
