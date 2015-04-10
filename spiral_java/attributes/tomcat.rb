110bdf93e9bee1e0c41762bfa31a296ff06afcec
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-10 14:41:01 +0800

    add basic tomcat atrributes

---
+++ spiral_java/attributes/tomcat.rb
@@ -0,0 +1,4 @@
+default['spiral']['tomcat']['series'] = '7' 
+default['spiral']['tomcat']['version'] = '7.0.59'
+
+default['spiral']['tomcat']['url'] = "http://apache.mirrors.pair.com/tomcat/tomcat-#{node['spiral']['tomcat']['series']}/v#{node['spiral']['tomcat']['version']}/bin/apache-tomcat-#{node['spiral']['tomcat']['version']}.tar.gz"
