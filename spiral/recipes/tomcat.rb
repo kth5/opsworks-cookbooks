9c0feb8113aacd24bfaba12bd28fe22195f2061f
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-10 15:39:55 +0800

    chown tomcat directory to tomcat user

--- spiral/recipes/tomcat.rb
+++ spiral/recipes/tomcat.rb
@@ -8,3 +8,16 @@ end
 execute 'tomcat_extract' do
   command "tar xzf /tmp/tomcat.tar.gz -C /opt/ && mv /opt/apache-tomcat-#{node['spiral']['tomcat']['version']} /opt/tomcat"
 end
+
+directory "/opt/tomcat" do
+  owner 'tomcat'
+  group node['spiral']['users']['group']
+  mode '0755'
+  action :create
+end
+
+supervisor_service 'tomcat' do
+  action :enable
+  autostart true
+  user 'tomcat'
+end
