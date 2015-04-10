b5824c84824106774da0ffdd97930960073ce0dc
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-10 11:41:37 +0800

    remove opswork's java in favor of a much simpler version installing Oracle JDK 7

---
+++ spiral_java/recipes/default.rb
@@ -0,0 +1,15 @@
+apt_repository "webupd8team-java-trusty" do
+  uri "http://ppa.launchpad.net/webupd8team/java/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "EEA14886"
+end
+
+execute 'accept_oracle_license' do
+  command 'echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections'
+end
+
+package 'oracle-java7-installer'
+package 'oracle-java7-unlimited-jce-policy'
+package 'oracle-java7-set-default'
