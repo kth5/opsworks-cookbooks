8516b01f28fb717f3715b2e59129d751ab0db642
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-13 16:35:10 +0800

    create simple NGINX vhost for teamcity

--- spiral/recipes/teamcity.rb
+++ spiral/recipes/teamcity.rb
@@ -10,3 +10,12 @@ link log_folder do
   to '/opt/tomcat/logs'
   not_if { ::File.exists?(log_folder) }
 end
+
+template '/etc/nginx/sites-available/teamcity' do
+  source 'nginx_proxy_pass.erb'
+  verify "nginx -t -c %{path}"
+  action :create
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
