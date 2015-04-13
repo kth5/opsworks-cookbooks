84d7fbb69581e9c8a5d22821ae2c4b7248c8f276
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-13 15:41:13 +0800

    add workaround for teamcity logfolder configuration issue

--- spiral/recipes/teamcity.rb
+++ spiral/recipes/teamcity.rb
@@ -1,5 +1,12 @@
 include_recipe 'spiral::tomcat'
 
+log_folder = '/opt/logs'
+
 remote_file '/opt/tomcat/webapps/TeamCity.war'
   source 'http://download.jetbrains.com/teamcity/TeamCity-9.0.3.war'
+end
+
+link log_folder do
+  to '/opt/tomcat/logs'
+  not_if { ::File.exists?(log_folder) }
 end
